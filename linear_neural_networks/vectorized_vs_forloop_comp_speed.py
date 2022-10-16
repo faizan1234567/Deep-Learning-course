6d33dd1c6ed49e2955518f7b39e655d2178fc3c6
faizan12345 <mohammadfaizantanoli@gmail.com>
2022-10-16 19:53:18 +0500

    moved file

---
+++ linear_neural_networks/vectorized_vs_forloop_comp_speed.py
@@ -0,0 +1,4 @@
+"""In this program, for loop implementation and vectorized implementation will be compared for implementing 
+deep neural networks. It turns out that vectorized implementation is much faster than for loop implementation, please see the
+demonstration below"""
+
