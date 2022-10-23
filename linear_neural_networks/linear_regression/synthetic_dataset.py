2be89f955aabc5e096ae99406d14f80c4ead4eeb
faizan12345 <mohammadfaizantanoli@gmail.com>
2022-10-23 23:09:42 +0500

    In the synthetic dataset code, data visulization features has been added.

--- linear_neural_networks/linear_regression/synthetic_dataset.py
+++ linear_neural_networks/linear_regression/synthetic_dataset.py
@@ -3,6 +3,7 @@
 import math
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
 
 def synthetic_data(w, b, num_samples):
     """generate synthetic for linear regression model design and training
@@ -25,9 +26,23 @@ if __name__ == "__main__":
     true_w = torch.tensor([2, -3.4])
     true_b = torch.tensor([3.4])
     n = 4
+
     features, targets = synthetic_data(true_w, true_b, 1000)
     print("features shape: {}, labels shape: {}".format(features.shape, targets.shape))
     print("fourth example:\n features: {}\n labels {}".format(features[n].numpy().squeeze(), targets[n].numpy().squeeze()))
+
+    print('--'*40)
+    print('Plotting the second features in the training set and labels ...')
+
+    plt.figure(figsize= (10, 5))
+    plt.scatter(features[:, 1].detach().numpy(), targets.detach().numpy(), 5, c= "g")
+    plt.xlabel('X')
+    plt.ylabel('f(x)')
+    plt.title('Synthetic dataset')
+    plt.grid()
+    plt.show()
+    
+    print('The end!!')
 
 
 
